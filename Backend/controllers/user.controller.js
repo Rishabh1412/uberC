30aa2b26f1e4c46ef773fbd20f203f060fac1028
Rish <rishabhkartik2004@gmail.com>
2024-11-30 19:52:22 +0530

    login added

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -34,3 +34,30 @@ module.exports.registerUser = async (req, res, next) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const token = user.generateAuthToken();
+
+        res.status(200).json({ token, user });
+    } catch (error) {
+        next(error);
+    }
+};
